6d7332fbd932fb545f81af6a3f1b1333c06b1c9b
JorgeGit_dev <jorgar2222@gmail.com>
2026-05-06 21:05:12 -0300

    Cambio por error importacion ItemDatail

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ItemDetail } from "../itemDetailContainer/ItemDetailContainer";
+import { ItemDetail } from "../itemDetail/ItemDetail";
 
 export const ItemDetailContainer = () => {
   const { id } = useParams();
